77af814784a7f10210076a0a17175ce91318faba
Shah Mohammad <shahiitp07@gmail.com>
2025-02-01 16:39:42 +0530

    chore: rename project folder structure from backendBhartFD to backendBharatFD

--- backendBharatFD/asgi.py
+++ backendBharatFD/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for faq_project project.
+ASGI config for backendBharatFD project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendBhartFD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendBharatFD.settings')
 
 application = get_asgi_application()
--- backendBharatFD/wsgi.py
+++ backendBharatFD/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for faq_project project.
+WSGI config for backendBharatFD project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendBhartFD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendBharatFD.settings')
 
 application = get_wsgi_application()
